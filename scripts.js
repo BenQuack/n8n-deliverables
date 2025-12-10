766778f72919f98adb03c3161c679d22c4f4c3b2
BenQuack <bquacken@uvm.edu>
2025-12-09 22:05:32 -0500

    big changes

--- scripts.js
+++ scripts.js
@@ -20,4 +20,38 @@ async function callSimpleWorkFlows(option){
     }
 }
 
+const workflows = {
+    1: 'workflows/rag-fp.json',
+    2: 'workflows/email chatbot.json',
+    3: 'workflows/telegram.json',
+    4: 'workflows/webscraper.json',
+    5: '',
+    6: '',
+}
+
+const workflowTitles = {
+    1: 'RAG Test for Final Project',
+    2: 'Email Chatbot',
+    3: 'Telegram Bot',
+    4: 'Web Scraper',
+    5: 'Workflow 5',
+    6: 'Workflow 6',
+}
+
+
+async function loadWorkflow(){
+    console.log('Loading workflow id: ', localStorage.getItem("selectedWorkflow"));
+    const demo = document.getElementById("demo");
+    const header = document.getElementById("workflowTitle");
+    const workflowId = localStorage.getItem("selectedWorkflow");
+    header.innerText = workflowTitles[workflowId];
+    const response = await fetch(workflows[workflowId]);
+    const workflow = await response.json();
+    demo.setAttribute('workflow', JSON.stringify(workflow));
+}
+
+function selectWorkflow(id){
+    localStorage.setItem("selectedWorkflow", id);
+}
+
 
